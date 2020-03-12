967345a4d717992fda072b5dad80813621e7acef
Antti Karvanen <antti.karvanen@exove.fi>
2020-03-12 16:55:11 +0200

    correctly use device with cuda

--- cifar-10.py
+++ cifar-10.py
@@ -152,7 +152,8 @@ correct = 0
 total = 0
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
+        #images, labels = data
+        images, labels = data[0].to(device), data[1].to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
